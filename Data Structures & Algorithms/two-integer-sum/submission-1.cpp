f31108ba69df3d2b5e8156cfb9ed96db0abe52c0
Sachin Tiwari <141061271+Sachin-1729@users.noreply.github.com>
2026-04-09 00:39:31 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans{};
+        map<int , int> mp{};
+        for(int i = 0; i < nums.size(); i++)
+        {
+            mp[target -nums[i]] = i;
+        }
+
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(mp[nums[i]] && mp[nums[i]] != i)
+            {
+                return {i , mp[nums[i]]};
+            }
+        }
+        return {};
+    }
+};
